9fac6065aaa91f1f8d53148bc0a80e906fe4b0b6
Junayd H <junaydhus@gmail.com>
2023-01-18 21:14:13 -0800

    implemented a function to check if a word exists in a 2D array both vertically or horizontally. Pair-programmed with @ethanw03

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,35 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return TRUE if the word is vertically present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'K', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not vertically present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isFalse(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let transposedArray = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    transposedArray.push([]);
+  }
+  for (let row = 0; row < letters.length; row++) {
+    for (let col = 0; col < letters[row].length; col++) {
+      transposedArray[col][row] = letters[row][col];
     }
-}
-
-module.exports = wordSearch
+  }
+  let verticalJoin = transposedArray.map((ls) => ls.join(""));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+module.exports = wordSearch;
